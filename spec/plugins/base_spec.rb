5ccf725605fc15f55978e5abb5f12f7bf52ac57f
Jeremy Ebler <jebler@gmail.com>
2013-01-19 14:54:31 -0800

    plugin-base specs

--- spec/plugins/base_spec.rb
+++ spec/plugins/base_spec.rb
@@ -21,17 +21,31 @@ plugins = [
 ]
 
 describe "Basic Plugin Test" do
-  plugins.each do |plugin|
 
+  describe Isis::Plugin::Base do
+    it "responds to methods" do
+      should respond_to(:receive_message)
+    end
+  end
+
+  plugins.each do |plugin|
     describe plugin do
       let(:speaker) { Random.speaker }
       let(:message) { Random.message }
+
+      it "is a subclass of Isis::Plugins::Base" do
+        plugin.superclass.should be Isis::Plugin::Base
+      end
+
+      it "responds to methods" do
+        should respond_to(:respond_to_msg?, :response)
+      end
 
       it "responds to receive_message with nil, [strings], or string" do
         subject.receive_message( message, speaker ).should be_a_plugin_response
       end
     end
-
   end
+
 end
 
